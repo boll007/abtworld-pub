60b91c1c6afcc004f3df4c99d2d32a99f31cfec6
helloabt <2439897034@qq.com>
2019-10-17 10:56:40 +0800

    Fix build error

--- src/libs/asset_pic.js
+++ src/libs/asset_pic.js
@@ -9,7 +9,7 @@ module.exports = [
     contact: 'qq:',
     owner_did: 'did:abt:z1XKKV5TykX467Dtro3F168Raq6eTF8tWGR',
     blur_src: '/static/images/payment/landscape/abtworld.jpg',
-    hd_src: '/static/images/payment/landscape/abtworld/59a53b90cc881dbc27c8ab2cf29bf32f.jpg,
+    hd_src: '/static/images/payment/landscape/abtworld/59a53b90cc881dbc27c8ab2cf29bf32f.jpg',
     asset_did: 'afba8d499da726ca45552f0c518f8fce4f3c12ca',
     link: '/payment?memo=afba8d499da726ca45552f0c518f8fce4f3c12ca',
     title: 'ABT世界',
