25be2c2a19fb042c715a50815dbe4404ac6ba96d
helloabt <2439897034@qq.com>
2019-10-17 10:46:40 +0800

    Abtworld source code initial

---
+++ src/libs/asset_pic.js
@@ -0,0 +1,22 @@
+//picture asset
+//small blur size: 25 * 25
+//asset_did&link: sha1sum <picture file>
+//hd_src: date |md5sum
+module.exports = [
+  {
+    category: 'landscape',
+    owner: 'abtworld',
+    contact: 'qq:',
+    owner_did: 'did:abt:z1XKKV5TykX467Dtro3F168Raq6eTF8tWGR',
+    blur_src: '/static/images/payment/landscape/abtworld.jpg',
+    hd_src: '/static/images/payment/landscape/abtworld/59a53b90cc881dbc27c8ab2cf29bf32f.jpg,
+    asset_did: 'afba8d499da726ca45552f0c518f8fce4f3c12ca',
+    link: '/payment?memo=afba8d499da726ca45552f0c518f8fce4f3c12ca',
+    title: 'ABT世界',
+    description: '重新定义个人资产价值',
+    worth: '18',
+    token_sym: 'TBA',
+    payback_rate: '0.6',
+  },
+];
+
